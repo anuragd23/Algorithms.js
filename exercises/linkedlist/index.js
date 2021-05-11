6f0b8d0b07d0540194c8b1c2a19c961b1e2ec694
Anurag Das <anurag.das23.ad@gmail.com>
2021-05-11 18:42:05 -0400

    Started Linked List

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,55 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
 
-class LinkedList {}
+    constructor(Data, Node = null) {
+        this.data = Data;
+        this.next = Node;
+    }
+}
+
+class LinkedList {
+
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        const headNode = new Node(data);
+        if(this.head) headNode.next = this.head;
+        this.head = headNode;
+    }
+
+    size() {
+        if (this.head === null) return 0;
+
+        let count = 1;
+        let tempNode = this.head;
+
+        while(tempNode.next) {
+            count++;
+            tempNode = tempNode.next;
+        }
+        return count;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let tempNode = this.head;
+
+        while(tempNode.next) {
+            tempNode = tempNode.next;
+        }
+        return tempNode;
+    }
+
+    clear() {
+        this.head = null;
+    }
+}
 
 module.exports = { Node, LinkedList };
