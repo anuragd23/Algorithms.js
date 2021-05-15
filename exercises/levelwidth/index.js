9c3a457181eee98acc978f851bc101a61594d8ae
Anurag Das <anurag.das23.ad@gmail.com>
2021-05-15 16:37:09 -0400

    Second attempt at Tree level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,24 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    let nodesLeft = [root,'levelEnd'];
+    let width = [0];
+
+    while (nodesLeft.length > 1) {
+        let node = nodesLeft.shift();
+
+        if(node === 'levelEnd') {
+            nodesLeft.push('levelEnd');
+            width.push(0);
+        }
+        else {
+            nodesLeft.push(...node.children);
+            width[width.length - 1]++; 
+        }
+    }
+
+    return width;
+}
 
 module.exports = levelWidth;
