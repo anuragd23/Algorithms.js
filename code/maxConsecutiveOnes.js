a12fabebf9d37dc39b7e57d85d01e9d31c52424d
Anurag Das <anurag.das23.ad@gmail.com>
2021-07-03 08:45:11 -0400

    added max consecutive ones description

--- code/maxConsecutiveOnes.js
+++ code/maxConsecutiveOnes.js
@@ -1,3 +1,6 @@
+
+//Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
 /**
  * @param {number[]} nums
  * @param {number} k
