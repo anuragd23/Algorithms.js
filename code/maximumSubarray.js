bc3211f68a01c32a95a7056238eafab8898aa341
Anurag Das <anurag.das23.ad@gmail.com>
2021-07-05 12:12:17 -0400

    added drescription to max subarray

--- code/maximumSubarray.js
+++ code/maximumSubarray.js
@@ -1,3 +1,6 @@
+//Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+
 /**
  * @param {number[]} nums
  * @return {number}
