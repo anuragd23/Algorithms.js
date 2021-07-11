27ef00d598684b4b025520d3ed3836aa26632b0c
Anurag Das <anurag.das23.ad@gmail.com>
2021-07-11 00:26:52 -0400

    Added fizzBuzz solution

--- code/fizzBuzz.js
+++ code/fizzBuzz.js
@@ -7,3 +7,20 @@ answer[i] == "Buzz" if i is divisible by 5.
 answer[i] == i if non of the above conditions are true.
 
 */
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+    
+    const result = [];
+    
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) result.push("FizzBuzz");
+        else if (i % 5 === 0) result.push("Buzz");
+        else if (i % 3 === 0) result.push("Fizz");
+        else result.push(i.toString());
+    }
+    return result;
+};
