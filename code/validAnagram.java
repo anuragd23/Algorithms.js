3841fc20336db9f9f98be125ac6026f41084348d
Anurag Das <anurag.das23.ad@gmail.com>
2021-08-14 10:03:10 -0400

    Added Valid Anagram Solution

--- code/validAnagram.java
+++ code/validAnagram.java
@@ -1,1 +1,24 @@
 //Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s == null || t == null || s.length() != t.length()) return false;
+        
+        int[] alphabets = new int[26];
+        
+        for(int i = 0; i < s.length(); i++) {
+            alphabets[s.charAt(i) - 'a']++;
+        }
+        
+        for(int i = 0; i < t.length(); i++) {
+            alphabets[t.charAt(i) - 'a']--;
+            if(alphabets[t.charAt(i) - 'a'] < 0) return false;
+        }
+        
+        for(int i = 0; i < alphabets.length; i++) {
+            if(alphabets[i] != 0) return false;
+        }
+        return true;
+    }
+}
