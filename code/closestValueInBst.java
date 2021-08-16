5d3066689aff05d84ef89b16ca39634c4416066c
Anurag Das <anurag.das23.ad@gmail.com>
2021-08-16 18:44:05 -0400

    solution added for the closest value in BST

--- code/closestValueInBst.java
+++ code/closestValueInBst.java
@@ -1,1 +1,31 @@
 //Given a binary search tree and a target node K. The task is to find the node with minimum absolute difference with given target value K.
+
+import java.util.*;
+
+class Program {
+  public static int findClosestValueInBst(BST tree, int target) {
+    // Write your code here.
+		BST currentNode = tree;
+		int closestValue = tree.value;
+		
+		while(currentNode != null) {
+			if(Math.abs(target - closestValue ) > Math.abs(target - currentNode.value)) {
+				closestValue  = currentNode.value;
+			}
+					
+			if(target > currentNode.value) currentNode = currentNode.right;
+			else currentNode = currentNode.left;
+		}
+    return closestValue;
+  }
+
+  static class BST {
+    public int value;
+    public BST left;
+    public BST right;
+
+    public BST(int value) {
+      this.value = value;
+    }
+  }
+}
