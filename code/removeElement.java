7ffe5d8ac9513184130752a3460c70e81fa7afe8
Anurag Das <anurag.das23.ad@gmail.com>
2021-10-07 17:25:44 -0400

    Remove element

---
+++ code/removeElement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int start = 0;
+        
+        for (int end = 0; end < nums.length; end++) {
+            if (nums[end] != val) {
+                nums[start] = nums[end];
+                start++;
+            }
+        }
+        
+        return start;
+    }
+}
