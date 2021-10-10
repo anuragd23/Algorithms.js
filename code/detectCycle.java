e2875ed7e834faa44b31448a0e10d5c484cb1402
Anurag Das <anurag.das23.ad@gmail.com>
2021-10-09 22:08:26 -0400

    Detect Cycle in a Linked List

---
+++ code/detectCycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if (head == null) return false;
+        
+        ListNode walker = head;
+        ListNode runner = head.next;
+        
+        while (walker != runner) {
+            if (runner == null || runner.next == null) return false;
+            walker = walker.next;
+            runner = runner.next.next;
+        }
+        
+        return true;
+    }
+}
